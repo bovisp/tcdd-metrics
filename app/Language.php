2f177bb294fb27de2e6a6c4edd78775d486200b8
Marshall <alexander.marshall@ec.gc.ca>
2019-03-13 11:55:00 -0600

    create language factory

--- app/Language.php
+++ app/Language.php
@@ -6,8 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Language extends Model
 {
-    public function badges() {
-        return $this->belongsToMany(Badge::class);
-        //define relationship to different table?
-    }
+    // public function badges() {
+    //     return $this->belongsToMany(Badge::class);
+    //     //define relationship to different table?
+    // }
+
+    // Schema::connection('mysql')->create('language', function (Blueprint $table) {
+    //     $table->increments('id');
+    //     $table->string('name');
+    // });
 }
--- tests/Feature/ManageBadgeLanguagesTest.php
+++ tests/Feature/ManageBadgeLanguagesTest.php
@@ -2,17 +2,28 @@
 
 namespace Tests\Feature;
 
+use App\Language;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ManageBadgeLanguagesTest extends TestCase
 {
-    /** test */
-    public function testExample()
+    /** @test */
+    public function a_user_can_assign_a_language_to_a_badge()
     {
-        $response = $this->get('/');
+        //create a language
+        $language = factory(Language::class)->create();
+        //create a badge
+        $badge = factory(Badge::class)->create();
 
-        $response->assertStatus(200);
+        $attributes = [
+            'language_id' => $language->id,
+            'badge_id' => $badge->id
+        ];
+        //post to badge-languages...or update badges/{id}...?
+        $this->post('/badge-languages', $attributes);
+
+        $this->assertDatabaseHas('badge_language', $attributes);
     }
 }
