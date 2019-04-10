3760f10ad007a8e53987840f8b9f99c7f170a5dc
alexjmarshall <marshalexander@gmail.com>
2019-04-10 14:34:44 -0600

    code clean up

--- app/Http/Controllers/BadgeLanguageController.php
+++ app/Http/Controllers/BadgeLanguageController.php
@@ -26,41 +26,58 @@ class BadgeLanguageController extends Controller
             'language_id' => 'exists:languages,id'
         ]);
 
-        $badgeId = DB::connection('mysql')->table('badge_language')->where(['id' => $badgeLanguageId])->select('badge_id')->get()->map(function ($badgeLanguage) {return $badgeLanguage->badge_id;})[0];
-        $badgeIssued = DB::connection('mysql2')->table('mdl_badge_issued')->where(['badgeid' => $badgeId])->exists();
+        $badgeId = DB::connection('mysql')->table('badge_language')
+            ->where(['id' => $badgeLanguageId])
+            ->select('badge_id')
+            ->get()
+            ->map(function ($badgeLanguage) {return $badgeLanguage->badge_id;})[0];
+
+        $badgeIssued = DB::connection('mysql2')->table('mdl_badge_issued')
+            ->where(['badgeid' => $badgeId])
+            ->exists();
 
         if($badgeIssued) {
-            return response('Could not update this badge\'s language. Badge has already been issued.', 422);
+            return response("Could not update this badge's language. Badge has already been issued.", 422);
         } else {
-            DB::connection('mysql')->table('badge_language')->where(['id' => $badgeLanguageId])
+            DB::connection('mysql')->table('badge_language')
+            ->where(['id' => $badgeLanguageId])
             ->update([
                 'badge_id' => request('badge_id'),
                 'language_id' => request('language_id')
             ]);
-            return response('Successfully updated this badge\'s language.', 200);
+
+            return response("Successfully updated this badge's language.", 200);
         }
     }
 
     public function destroy($badgeLanguageId) {
-        $badgeId = DB::connection('mysql')->table('badge_language')->where(['id' => $badgeLanguageId])->select('badge_id')->get()->map(function ($badgeLanguage) {return $badgeLanguage->badge_id;})[0];
-        $badgeIssued = DB::connection('mysql2')->table('mdl_badge_issued')->where(['badgeid' => $badgeId])->exists();
+        $badgeId = DB::connection('mysql')->table('badge_language')
+            ->where(['id' => $badgeLanguageId])
+            ->select('badge_id')
+            ->get()
+            ->map(function ($badgeLanguage) {return $badgeLanguage->badge_id;})[0];
+
+        $badgeIssued = DB::connection('mysql2')->table('mdl_badge_issued')
+            ->where(['badgeid' => $badgeId])
+            ->exists();
 
         if($badgeIssued) {
-            return response('Could not delete this badge\'s language. Badge has already been issued.', 422);
+            return response("Could not delete this badge's language. Badge has already been issued.", 422);
         } else {
-            DB::connection('mysql')->table('badge_language')->delete([
-                'id' => $badgeLanguageId
-            ]);
+            DB::connection('mysql')->table('badge_language')
+                ->delete([
+                    'id' => $badgeLanguageId
+                ]);
             return response('Successfully deleted this badge\'s language.', 200);
         }
     }
 
     public function index() {
         return DB::connection('mysql')->table('badge_language')
-        ->join('moodledb.mdl_badge', 'badge_language.badge_id', '=', 'moodledb.mdl_badge.id')
-        ->join('languages', 'badge_language.language_id', '=', 'languages.id')
-        ->select('badge_language.id', 'moodledb.mdl_badge.id as badge_id', 'moodledb.mdl_badge.name as badge_name', 'languages.id as language_id', 'languages.name as language_name')
-        ->orderBy('badge_language.id', 'asc')
-        ->get();
+            ->join('moodledb.mdl_badge', 'badge_language.badge_id', '=', 'moodledb.mdl_badge.id')
+            ->join('languages', 'badge_language.language_id', '=', 'languages.id')
+            ->select('badge_language.id', 'moodledb.mdl_badge.id as badge_id', 'moodledb.mdl_badge.name as badge_name', 'languages.id as language_id', 'languages.name as language_name')
+            ->orderBy('badge_language.id', 'asc')
+            ->get();
     }
 }
