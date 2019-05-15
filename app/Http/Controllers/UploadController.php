5c22be53fd59dc2f596bfba5fde55cd4824a13c4
alexjmarshall <marshalexander@gmail.com>
2019-05-15 15:15:14 -0600

    update controller to save file to disk

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -18,17 +18,20 @@ class UploadController extends Controller
         ]);
     }
 
-    public function upload() {
+    public function upload(Request $request) {
+        $path = $request->file('file')->store('/');
+
+        return json_encode([
+            'data' => [
+                'success' => true,
+                'path' => $path
+            ]
+        ]);
         //return response("Successfully updated this badge's language.", 500);
 
         // if (isset($_FILES['file'], $_POST['id'])) {
         //     move_uploaded_file($_FILES['file']['tmp_name'], 'C:\Users\MarshallAl\Downloads' . $_POST['id']);
         // }
 
-        echo json_encode([
-            'data' => [
-                'success' => true,
-            ]
-        ]);
     }
 }
